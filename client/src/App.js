3a8d773e60725d606f362eb14375ab6fd8f7eae1
Hamza Aamir <hamzaamirm2001@gmail.com>
2022-06-25 01:29:49 +0500

    Add Read Done

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,35 @@
 import './App.css';
+import React, {useState, useEffect} from 'react';
+import Axios from 'axios'
 
 function App() {
+
+  const handleSubmit = () => {
+    Axios.post("http://localhost:3001/insert", {
+      todoTitle: todo,
+    });
+  }
+
+  useEffect(() => {
+    Axios.get("http://localhost:3001/read").then((response) => {
+      setTodoList(response.data)
+    });
+  }, [])
+
+  const [todo, setTodo] = useState("");
+  const [todoList, setTodoList] = useState([]);
+
   return (
     <div>
-      
+      <h1>MERN TodoList</h1>
+      <input type="text" onChange={e => setTodo(e.target.value)} />
+      <button onClick={handleSubmit}>Add Todo</button>
+      <h1>Data</h1>
+      <ol>
+        {todoList.map((todo, key) => {
+          return <li key={key}>{todo.todoTitle} (Date Created: {todo.todoDate})</li>
+        })}
+      </ol>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,17 +1,20 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors")
 const TodoModel = require("./models/Todo")
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 mongoose.connect("mongodb+srv://root:root@cluster0.gh8v8b6.mongodb.net/todolist",
 {
     useNewUrlParser: true,
 });
 
-app.get("/", async (req, res) => {
-    const todos = new TodoModel({todoTitle: "dummy title", todoDate: 24});
+app.post("/insert", async (req, res) => {
+    const todoTitle = req.body.todoTitle;
+    const todos = new TodoModel({todoTitle: todoTitle, todoDate: 24});
 
     try{
         await todos.save();
@@ -20,6 +23,15 @@ app.get("/", async (req, res) => {
     catch (err) {
         console.log(err);
     }
+});
+
+app.get("/read", async (req, res) => {
+    TodoModel.find({}, (err, result) => {
+        if(err){
+            res.send(err);
+        }
+        res.send(result);
+    })
 })
 
 app.listen(3001, () => {
